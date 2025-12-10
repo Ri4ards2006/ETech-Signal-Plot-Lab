65d30da67dacf304f859b2dc976adeb9aa55795f
Richard Zuikov <astroricards@gmail.com>
2025-12-10 23:04:42 +0100

    for for for for if if if if you Pass the x line i will remember and iterate (kys)

--- C++/Freq_Analyzer/Analyzer.h
+++ C++/Freq_Analyzer/Analyzer.h
@@ -16,7 +16,16 @@ public:
      * Also den vector von Doubles namens Samples und die Samples pro sekunde oder ? und die wird zu f umgerechnet nh?
      */
     static double zeroCrossingFreq(const std::vector<double>& samples, double Fs) {
+int zeroCrossing = 0;  // Hier wird die Int Var namens zeroCrossing initiert
+        for (size_t i = 1; i < samples.size(); ++i ) { // Ist size_t ei eine var oder was genau ich checke es nicht ?
+            // Denn dann wird sie
+if ((samples[i-1] < 0 && samples[i] >= 0 ) || (samples[i-1] > 0 && samples[i] <= 0)) {
 
+
+
+}
+
+        }
     }
 
 
