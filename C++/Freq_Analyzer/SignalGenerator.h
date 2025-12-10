678b12a2128c00e85cc531c61c29c29d59461dea
Richard Zuikov <astroricards@gmail.com>
2025-12-10 22:36:54 +0100

    Literally Losing my Mind here (Help)

--- C++/Freq_Analyzer/SignalGenerator.h
+++ C++/Freq_Analyzer/SignalGenerator.h
@@ -4,6 +4,12 @@
 
 #ifndef FREQ_ANALYZER_SIGNALGENERATOR_H
 #define FREQ_ANALYZER_SIGNALGENERATOR_H
+//-_----------------------------- brAH ES GEHT LOS
+// HEADER FILE (BALD KRIEGE ICH AUCH EINE KUGEL IN MEIN HEAD)
+
+#pragma once  // Was zum dick heißt das pragma ? Dota 2 Klasse ?
+#include <vector>
+#include <cmath>  // Wie kann ich eig nochmal cmath einsehen brah ?
 
 
 class SignalGenerator {
