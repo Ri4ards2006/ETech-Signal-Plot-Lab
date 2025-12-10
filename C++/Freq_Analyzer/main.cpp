12392e3baf37c29a22f6eb945aa4e3a02c18f827
Richard Zuikov <astroricards@gmail.com>
2025-12-10 23:51:10 +0100

    Yeah kinda done i am done help me pls

--- C++/Freq_Analyzer/main.cpp
+++ C++/Freq_Analyzer/main.cpp
@@ -25,4 +25,12 @@ const double Fs = 1000.0; // sampling rate
 
     double f_est = Analyzer :: zeroCrossingFreq(samples,Fs); // Initierung der f est variable und Zero Cross methode oder iwie so hiilfe
     std::cout << "Estimated Frequency (Zero Crossing ):"<< f_est ;  // Was ist dieses dieses <<<<<< eig digga
+
+auto spectrum = Analyzer :: computeDFT(samples); // Nur der vec aufruf der deklarierten methode computeDFT vom header  vom Analyzer
+ for (size_t k = 0; k < 20; k++) { // Wie viele und wie genau oder ???
+double freq = k* Fs/ N ; // für Omega oder ?
+     std::cout << "f=" << freq << "Hz, Magnitude" << spectrum[k] << "\n"; // Digga warum << statt + ?????????ßß
+ }
+    return 0; // ist gut nh wie die chance wie ka kys
 }
+
