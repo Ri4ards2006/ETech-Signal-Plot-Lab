d64eccdd4591595a98aa2e2be32b5098e085d399
Richard Zuikov <astroricards@gmail.com>
2025-12-11 12:47:47 +0100

    Fixed the Childish Errors...

--- C++/Freq_Analyzer/ADCBuffer.h
+++ C++/Freq_Analyzer/ADCBuffer.h
@@ -10,10 +10,19 @@
 class ADCBuffer {
 private:
     static const int SIZE = 128; // Warum isst es so groß Geschrieben wtf ???? und warum statisch
-    uint16_t buffer[SiZE]; // Was ist der datentyp und ist es jz ein array oder wtf ?????
-     int index =0
-};
+    uint16_t buffer[SIZE]; // Was ist der datentyp und ist es jz ein array oder wtf ?????
+    int index =0;
+
 public:
-void addSample(uint16_t val) // Was ist dieses val jz ? ist es der name davon es ist so fking komisch
+    void addSample(uint16_t val) /* Was ist dieses val jz ? ist es der name davon es ist so fking komisch */ {
+
+
+    }
+
+
+
+
+
+};
 
 #endif //FREQ_ANALYZER_ADCBUFFER_H
