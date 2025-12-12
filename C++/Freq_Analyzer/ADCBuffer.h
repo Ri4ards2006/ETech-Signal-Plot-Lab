3c5fad2f3d99df45f0ddd09e7223b43679b703ec
Richard Zuikov <astroricards@gmail.com>
2025-12-12 18:14:52 +0100

    Trying to do the ADC Buffer more Deterministic

--- C++/Freq_Analyzer/ADCBuffer.h
+++ C++/Freq_Analyzer/ADCBuffer.h
@@ -23,7 +23,26 @@ buffer[index++] =  val; // Inwiefern ?? ist es denn jz das problem das er den we
     uint16_t geSample(int i ){ return buffer[i];}
 };
 
-#endif //FREQ_ANALYZER_ADCBUFFER_H
+#endif FREQ_ANALYZER_ADCBUFFER_H
 
 // Mir wurde noch explizit gesagt das es da keine Heap Allocation und keine eigene Header ???
-// heap allocation heißt noch eig  das es einf deterministische Ram belegung ist
+/*
+class ADCBuffer {
+private:
+    static const int SIZE = 128; // Warum isst es so groß Geschrieben wtf ???? und warum statisch
+    uint16_t buffer[SIZE]; // Was ist der datentyp und ist es jz ein array oder wtf ?????
+    int index =0;
+
+public:
+    void addSample(uint16_t val) /* Was ist dieses val jz ? ist es der name davon es ist so fking komisch */ {
+
+  //  buffer[index++] =  val; // Inwiefern ?? ist es denn jz das problem das er den werd um einen erhöht also die variable oder wie
+ //   // Und warum immer []  es verwirrt mich
+  //  if (index >= SIZE) index = 0;  // Warum darf mann das ?
+//}
+//uint16_t geSample(int i ){ return buffer[i];}
+//};
+
+//#endif //FREQ_ANALYZER_ADCBUFFER_H
+
+//heap allocation heißt noch eig  das es einf deterministische Ram belegung ist
