47bb24d27c41d264dd0bacd895c301ada378da52
Richard Zuikov <astroricards@gmail.com>
2025-12-12 18:24:18 +0100

    Apored

--- C++/Freq_Analyzer/ADCBuffer.h
+++ C++/Freq_Analyzer/ADCBuffer.h
@@ -7,30 +7,40 @@
 #include <cstdint>
 
 
-
-
-
-
-#endif FREQ_ANALYZER_ADCBUFFER_H
-
-// Mir wurde noch explizit gesagt das es da keine Heap Allocation und keine eigene Header ???
-/*
 class ADCBuffer {
 private:
     static const int SIZE = 128; // Warum isst es so groß Geschrieben wtf ???? und warum statisch
-    uint16_t buffer[SIZE]; // Was ist der datentyp und ist es jz ein array oder wtf ?????
-    int index =0;
+ //   uint16_t buffer[SIZE]; // Was ist der datentyp und ist es jz ein array oder wtf ?????
+  //  int index =0;
 
 public:
-    void addSample(uint16_t val) /* Was ist dieses val jz ? ist es der name davon es ist so fking komisch */ {
+   static const uint16_t SIZE = 128;
 
-  //  buffer[index++] =  val; // Inwiefern ?? ist es denn jz das problem das er den werd um einen erhöht also die variable oder wie
- //   // Und warum immer []  es verwirrt mich
-  //  if (index >= SIZE) index = 0;  // Warum darf mann das ?
-//}
-//uint16_t geSample(int i ){ return buffer[i];}
-//};
+    void addSample(uint16_t val) {
+        buffer[index]
+    }
 
-//#endif //FREQ_ANALYZER_ADCBUFFER_H
+
+
+
+
+
+
+
+
+
+
+    //void addSample(uint16_t val) /* Was ist dieses val jz ? ist es der name davon es ist so fking komisch */ {
+
+//buffer[index++] =  val; // Inwiefern ?? ist es denn jz das problem das er den werd um einen erhöht also die variable oder wie
+        // Und warum immer []  es verwirrt mich
+   //     if (index >= SIZE) index = 0;  // Warum darf mann das ?
+  //  }
+  //  uint16_t geSample(int i ){ return buffer[i];}
+};
+
+#endif //FREQ_ANALYZER_ADCBUFFER_H
+
+// Mir wurde noch explizit gesagt das es da keine Heap Allocation und keine eigene Header ???
 
 //heap allocation heißt noch eig  das es einf deterministische Ram belegung ist
