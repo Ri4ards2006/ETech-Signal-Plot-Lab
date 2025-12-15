0d0c45dbcab7daf1e55e23f3a787b6cf8ff425e9
Richard Zuikov <astroricards@gmail.com>
2025-12-15 13:51:36 +0100

    Implementation of the  RingBuffer Logik with an Push method

--- C++/Freq_Analyzer/src/buffer/RingBuffer.h
+++ C++/Freq_Analyzer/src/buffer/RingBuffer.h
@@ -5,28 +5,43 @@
 #ifndef FREQ_ANALYZER_RINGBUFFER_H
 #define FREQ_ANALYZER_RINGBUFFER_H
 
-#include <cstdint> // Was ist das für eine Lib Genau ????
+#include <cstdint> // Die lib definiert die genaue größe von Int Typen
+// Es ist wichtig damit die Fest geräteübergreifend sind..
+// uint16_t ist klar und deterministisch !!!
+template <typename T,  uint16_t SIZE> //Es ist ein KompilierZeit Konstant Wert
+// von dem typ uint16_t
 
-template <typename T,  uint16_t SIZE> // Initiere einen Template für all den
-// Komischen kram.. ,  also die jeweilige größe ist ja 16 Bit aber was dast ???
-// Steht das mit der  lib in verbindung ?
-
-// Achso typ und feste größe zur Compile-Zeit
-// "SIZE"  Quasi ein Const für den Kram
 class RingBuffer {
 
-    T Buffer[SIZE]; // Eine Fest Größe für die klasse die hier Privat
+    T Buffer[SIZE]; // An Real C Array
     // Deklariert wird
     volatile uint16_t head;// Die Schreibposition also der Kopf
     volatile uint16_t tail; // Leseposition
-    volatile bool full; // Heißt das nichts optimiert und verbessert wird.
+
+    volatile bool full; // Heißt das es außerhalb des codes Ändern kann
+    // Also darf der Compiler es nicht cachen und muss es jedes mall neu  Lesen
+public:
+    RingBuffer() : head(0), tail(0), full(false) {}
+    // Also jz würde das Objekt halt mit einem Guten Heap management Erzeugt werden
+
+
+    bool push(T& value) // effiziente benutzung von C++ Space
+    {
+        if (full) {
+            return false;
+        }
+
+        buffer[head] = value;
+        head = (head + 1) % SIZE;
+
+        if (head == tail) {
+            full = true;
+        }
+        return true;
+    }
 };  // Volatile ISR Schreibt
 // Main Loop Liest also wird nix vom compiler optimiert
 
 // Constructor :
- public:
-RingBuffer() : head(0), tail(0), full(false) {}
-// Also jz würde das Objekt halt mit einem Guten Heap management Erzeugt werden
-
 
 #endif //FREQ_ANALYZER_RINGBUFFER_H
