760d74dced6f133270ec6e3e5e249782f08e0fd7
Richard Zuikov <astroricards@gmail.com>
2025-12-15 12:35:47 +0100

    Rearranging the Buffer code and making it more Deterministic with no "Self Proclaimed Efficiency"

--- C++/Freq_Analyzer/src/buffer/RingBuffer.h
+++ C++/Freq_Analyzer/src/buffer/RingBuffer.h
@@ -5,8 +5,21 @@
 #ifndef FREQ_ANALYZER_RINGBUFFER_H
 #define FREQ_ANALYZER_RINGBUFFER_H
 
+#include <cstdint> // Was ist das für eine Lib Genau ????
 
+template <typename T,  uint16_t SIZE> // Initiere einen Template für all den
+// Komischen kram.. ,  also die jeweilige größe ist ja 16 Bit aber was dast ???
+// Steht das mit der  lib in verbindung ?
+
+// Achso typ und feste größe zur Compile-Zeit
+// "SIZE"  Quasi ein Const für den Kram
 class RingBuffer {
+
+    T Buffer[SIZE]; // Eine Fest Größe für die klasse die hier Privat
+    // Deklariert wird
+    volatile uint16_t head;// Die Schreibposition also der Kopf
+    volatile uint16_t tail; // Leseposition
+    volatile bool full; // Heißt das nichts optimiert und verbessert wird.
 };
 
 
