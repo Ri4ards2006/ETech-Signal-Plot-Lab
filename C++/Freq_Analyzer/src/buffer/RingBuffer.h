7ba26f2d76f7409844fe776faa98cd76454f6d1a
Richard Zuikov <astroricards@gmail.com>
2025-12-15 12:24:42 +0100

    Initiated The Ring Buffer Class and Making it Deterministic

---
+++ C++/Freq_Analyzer/src/buffer/RingBuffer.h
@@ -0,0 +1,13 @@
+//
+// Created by richard on 15.12.25.
+//
+
+#ifndef FREQ_ANALYZER_RINGBUFFER_H
+#define FREQ_ANALYZER_RINGBUFFER_H
+
+
+class RingBuffer {
+};
+
+
+#endif //FREQ_ANALYZER_RINGBUFFER_H
