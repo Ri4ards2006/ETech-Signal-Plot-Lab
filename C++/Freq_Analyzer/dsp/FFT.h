7046db43a4ed217fa4479185dea2d80b56e30173
Richard Zuikov <astroricards@gmail.com>
2025-12-15 11:51:26 +0100

    Rearranging the New Classes o "Still" Scale this Whole Project

---
+++ C++/Freq_Analyzer/dsp/FFT.h
@@ -0,0 +1,13 @@
+//
+// Created by richard on 15.12.25.
+//
+
+#ifndef FREQ_ANALYZER_FFT_H
+#define FREQ_ANALYZER_FFT_H
+
+
+class FFT {
+};
+
+
+#endif //FREQ_ANALYZER_FFT_H
---
+++ C++/Freq_Analyzer/dsp/Window.h
@@ -0,0 +1,13 @@
+//
+// Created by richard on 15.12.25.
+//
+
+#ifndef FREQ_ANALYZER_WINDOW_H
+#define FREQ_ANALYZER_WINDOW_H
+
+
+class Window {
+};
+
+
+#endif //FREQ_ANALYZER_WINDOW_H
